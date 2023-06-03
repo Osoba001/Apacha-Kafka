057c914c83d8f8de640d8bc6d10c86e12ca0583c
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-06-03 16:20:05 +0100

    Setup basic apache consumer

---
+++ ApacheKafkaProducer/ApacheKafkaProducer/Program.cs
@@ -0,0 +1,23 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ProducerConfig{BootstrapServers = "localhost:9092"};
+
+using var producer=new ProducerBuilder<Null, string>(config).Build();
+
+try
+{
+	string? product;
+	while ((product = Console.ReadLine()) != null)
+	{
+		var resp = await producer.ProduceAsync("order-topic",
+			new Message<Null, string> { Value = JsonConvert.SerializeObject(new Order(product, 4)) }
+			);
+		Console.WriteLine(resp.Value);
+	}
+}
+catch (ProduceException<Null,string> exe)
+{
+	Console.WriteLine(exe.Message);
+}
+public record Order(string product, int quantity);
